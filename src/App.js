94f75ff0f2d3008660d75c8a9c90104f165b757e
Akbar Priyo Santosa <priyoakbar2942@gmail.com>
2023-05-26 15:49:38 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-<<<<<<< HEAD
 import NavContainer from './components/NavContainer';
 import Homepage from './pages/Homepage';
 
@@ -10,18 +9,6 @@ function App() {
       <NavContainer/>
       <Homepage/>
     </>
-=======
-import NavScroll from './components/NavScroll';
-import HomePage from './pages/HomePage';
-
-
-function App() {
-  return (
-   <>
-    <NavScroll/>
-    <HomePage/>
-   </>
->>>>>>> a22855d0d0b2f3280045489f8852bd98033cdfdf
   );
 }
 
