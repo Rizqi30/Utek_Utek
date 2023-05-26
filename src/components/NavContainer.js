da2b6da91e5d0cd5b0678518f764005ef4753f22
msadhimb <adhimbaqy72@gmail.com>
2023-05-26 20:55:34 +0700

    fix(navbar): fix a few bug in navbar menu

--- src/components/NavContainer.js
+++ src/components/NavContainer.js
@@ -1,36 +1,38 @@
-import logo from '../assets/logo.png'
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import logo from "../assets/logo.png";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function NavContainer() {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#" className='d-flex' style={{alignItems:'center'}}>
-        <img src={logo} className="logo_navbar" alt="" />
-        <h3>DIBOOKING</h3>
+        <Navbar.Brand
+          href="#"
+          className="d-flex"
+          style={{ alignItems: "center" }}
+        >
+          <img src={logo} className="logo_navbar" alt="" />
+          <h3>DIBOOKING</h3>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll" >
-          <div className='d-flex justify-content-center' style={{width:'100%'}}>
-          <Nav
-            className='nav'
-            style={{ maxHeight: '100px'}}
-            navbarScroll
-          >
+        <Navbar.Toggle id="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          {/* beberapa class tak hapus karena marai navbar e posisi ne ra nggenahb */}
+          <Nav className="mx-auto text-center">
             <Nav.Link href="#action1">Promosi</Nav.Link>
             <Nav.Link href="#action2">Rekomendasi</Nav.Link>
             <Nav.Link href="#action2">Kategori</Nav.Link>
             <Nav.Link href="#action2">Tentang Kami</Nav.Link>
-
           </Nav>
-          </div>
-          <Form className="d-flex">
-            <Button className='mx-3' variant="outline-success">Search</Button>
-            <Button variant="outline-success">Search</Button>
+          <Form className="d-flex justify-content-center">
+            <Button className="mx-2" variant="outline-success">
+              Search
+            </Button>
+            <Button className="mx-2" variant="outline-success">
+              Search
+            </Button>
           </Form>
         </Navbar.Collapse>
       </Container>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -11,9 +11,16 @@ import { Card } from "react-bootstrap";
 function HomePage() {
   return (
     <div>
-      <div className="d-flex flex-row mt-3 mb-3 px-5 justify-content-end align-items-center" style={{backgroundColor:'#2F2E41',height:'64px',color:'white'}}>
-        <p style={{margin:'0',marginRight:'10px'}}>Lokasi : </p>
-        <NavDropdown title= <span style={{color:'white'}}>Jakarta</span>  id="basic-nav-dropdown">
+      {/* iki mt-3 tak ilangi mergo navbar e ono warnane, nek dikei mt-3 ono gep warna putih ng tengah tengah */}
+      <div
+        className="d-flex flex-row mb-3 px-5 justify-content-end align-items-center"
+        style={{ backgroundColor: "#2F2E41", height: "64px", color: "white" }}
+      >
+        <p style={{ margin: "0", marginRight: "10px" }}>Lokasi : </p>
+        <NavDropdown
+          title={<span style={{ color: "white" }}>Jakarta</span>}
+          id="basic-nav-dropdown"
+        >
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
@@ -30,7 +37,8 @@ function HomePage() {
         }}
       >
         <Carousel.Item style={{ height: "100%", width: "100%" }}>
-          <img className="d-block w-100" src={sale} alt="First slide" />
+          {/* iki aku nambahi p-2 nganggo jeda antara gambar ben ra mepet, nek meh kanan kiri px-2 nek meh atas bawah py-2 */}
+          <img className="d-block w-100 p-2" src={sale} alt="First slide" />
         </Carousel.Item>
       </Carousel>
 
@@ -46,115 +54,193 @@ function HomePage() {
         <h3 className="mb-4">Rekomendasi Lapangan</h3>
         <div
           className="wrapper"
-          style={{width: "100%",padding:'10px',display:'flex',gap:'15px',justifyContent:'center',alignItems:'center',flexWrap:'wrap'}}
+          style={{
+            width: "100%",
+            padding: "10px",
+            display: "flex",
+            gap: "15px",
+            justifyContent: "center",
+            alignItems: "center",
+            flexWrap: "wrap",
+          }}
         >
-          <Card style={{ maxWidth: "18rem",border:'0'}}>
-            <Card.Img variant="top" src={lpng} style={{borderRadius:'5px',maxHeight:'165px',maxWidth:'278px'}}/>
-            <Card.Body style={{padding:'0'}}>
-              <Card.Text><p>Jaksel Futsal</p></Card.Text>
+          <Card style={{ maxWidth: "18rem", border: "0" }}>
+            <Card.Img
+              variant="top"
+              src={lpng}
+              style={{
+                borderRadius: "5px",
+                maxHeight: "165px",
+                maxWidth: "278px",
+              }}
+            />
+            <Card.Body style={{ padding: "0" }}>
               <Card.Text>
-                <h5>
-                  RP 75.000
-                </h5>
+                <p>Jaksel Futsal</p>
               </Card.Text>
               <Card.Text>
-                <p>
-                  Jakarta Selatan
-                </p>
-              </Card.Text><Card.Text>
-                <p>
-                  4.8 | Tersewa
-                </p>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card style={{ maxWidth: "18rem",border:'0'}}>
-            <Card.Img variant="top" src={lpng} style={{borderRadius:'5px',maxHeight:'165px',maxWidth:'278px'}}/>
-            <Card.Body style={{padding:'0'}}>
-              <Card.Text><p>Jaksel Futsal</p></Card.Text>
-              <Card.Text>
-                <h5>
-                  RP 75.000
-                </h5>
+                <h5>RP 75.000</h5>
               </Card.Text>
               <Card.Text>
-                <p>
-                  Jakarta Selatan
-                </p>
-              </Card.Text><Card.Text>
-                <p>
-                  4.8 | Tersewa
-                </p>
+                <p>Jakarta Selatan</p>
+              </Card.Text>
+              <Card.Text>
+                <p>4.8 | Tersewa</p>
               </Card.Text>
             </Card.Body>
           </Card>
-          <Card style={{ maxWidth: "18rem",border:'0'}}>
-            <Card.Img variant="top" src={lpng} style={{borderRadius:'5px',maxHeight:'165px',maxWidth:'278px'}}/>
-            <Card.Body style={{padding:'0'}}>
-              <Card.Text><p>Jaksel Futsal</p></Card.Text>
+          <Card style={{ maxWidth: "18rem", border: "0" }}>
+            <Card.Img
+              variant="top"
+              src={lpng}
+              style={{
+                borderRadius: "5px",
+                maxHeight: "165px",
+                maxWidth: "278px",
+              }}
+            />
+            <Card.Body style={{ padding: "0" }}>
               <Card.Text>
-                <h5>
-                  RP 75.000
-                </h5>
+                <p>Jaksel Futsal</p>
               </Card.Text>
               <Card.Text>
-                <p>
-                  Jakarta Selatan
-                </p>
-              </Card.Text><Card.Text>
-                <p>
-                  4.8 | Tersewa
-                </p>
+                <h5>RP 75.000</h5>
+              </Card.Text>
+              <Card.Text>
+                <p>Jakarta Selatan</p>
+              </Card.Text>
+              <Card.Text>
+                <p>4.8 | Tersewa</p>
               </Card.Text>
             </Card.Body>
           </Card>
-          <Card style={{ maxWidth: "18rem",border:'0'}}>
-            <Card.Img variant="top" src={lpng} style={{borderRadius:'5px',maxHeight:'165px',maxWidth:'278px'}}/>
-            <Card.Body style={{padding:'0'}}>
-              <Card.Text><p>Jaksel Futsal</p></Card.Text>
+          <Card style={{ maxWidth: "18rem", border: "0" }}>
+            <Card.Img
+              variant="top"
+              src={lpng}
+              style={{
+                borderRadius: "5px",
+                maxHeight: "165px",
+                maxWidth: "278px",
+              }}
+            />
+            <Card.Body style={{ padding: "0" }}>
               <Card.Text>
-                <h5>
-                  RP 75.000
-                </h5>
+                <p>Jaksel Futsal</p>
               </Card.Text>
               <Card.Text>
-                <p>
-                  Jakarta Selatan
-                </p>
-              </Card.Text><Card.Text>
-                <p>
-                  4.8 | Tersewa
-                </p>
+                <h5>RP 75.000</h5>
+              </Card.Text>
+              <Card.Text>
+                <p>Jakarta Selatan</p>
+              </Card.Text>
+              <Card.Text>
+                <p>4.8 | Tersewa</p>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card style={{ maxWidth: "18rem", border: "0" }}>
+            <Card.Img
+              variant="top"
+              src={lpng}
+              style={{
+                borderRadius: "5px",
+                maxHeight: "165px",
+                maxWidth: "278px",
+              }}
+            />
+            <Card.Body style={{ padding: "0" }}>
+              <Card.Text>
+                <p>Jaksel Futsal</p>
+              </Card.Text>
+              <Card.Text>
+                <h5>RP 75.000</h5>
+              </Card.Text>
+              <Card.Text>
+                <p>Jakarta Selatan</p>
+              </Card.Text>
+              <Card.Text>
+                <p>4.8 | Tersewa</p>
               </Card.Text>
             </Card.Body>
           </Card>
         </div>
       </section>
       <section
-      className="mt-5"
-      style={{
-        display: 'flex',
-        height: "fit-content",
-        flexDirection: 'column',
-        maxWidth: "1200px", 
-        margin: "auto",
-        justifyContent:'center',
-        padding: "15px",
-      }}
-    >
-      <h3 style={{textAlign:'center', marginBottom:'20px'}}>
-        Kategori Lapangan
-      </h3>
-      <div className="cat_wrapper d-flex justify-content-evenly " style={{gap:'10px',justifyContent:''}}>
-      <img src={bola} style={{boxShadow:'4px 6px 10px rgba(0, 0, 0, 0.25)',borderRadius:'50%'}} className="" alt="" />
-      <img src={bola} style={{boxShadow:'4px 6px 10px rgba(0, 0, 0, 0.25)',borderRadius:'50%'}} className="" alt="" />
-      <img src={bola} style={{boxShadow:'4px 6px 10px rgba(0, 0, 0, 0.25)',borderRadius:'50%'}} className="" alt="" />
-      <img src={bola} style={{boxShadow:'4px 6px 10px rgba(0, 0, 0, 0.25)',borderRadius:'50%'}} className="" alt="" />
-      <img src={bola} style={{boxShadow:'4px 6px 10px rgba(0, 0, 0, 0.25)',borderRadius:'50%'}} className="" alt="" />
-      <img src={bola} style={{boxShadow:'4px 6px 10px rgba(0, 0, 0, 0.25)',borderRadius:'50%'}} className="" alt="" />
-      </div>
-
-    </section>
+        className="mt-5"
+        style={{
+          display: "flex",
+          height: "fit-content",
+          flexDirection: "column",
+          maxWidth: "1200px",
+          margin: "auto",
+          justifyContent: "center",
+          padding: "15px",
+        }}
+      >
+        <h3 style={{ textAlign: "center", marginBottom: "20px" }}>
+          Kategori Lapangan
+        </h3>
+        <div
+          className="cat_wrapper d-flex justify-content-evenly "
+          style={{ gap: "10px", justifyContent: "" }}
+        >
+          <img
+            src={bola}
+            style={{
+              boxShadow: "4px 6px 10px rgba(0, 0, 0, 0.25)",
+              borderRadius: "50%",
+            }}
+            className=""
+            alt=""
+          />
+          <img
+            src={bola}
+            style={{
+              boxShadow: "4px 6px 10px rgba(0, 0, 0, 0.25)",
+              borderRadius: "50%",
+            }}
+            className=""
+            alt=""
+          />
+          <img
+            src={bola}
+            style={{
+              boxShadow: "4px 6px 10px rgba(0, 0, 0, 0.25)",
+              borderRadius: "50%",
+            }}
+            className=""
+            alt=""
+          />
+          <img
+            src={bola}
+            style={{
+              boxShadow: "4px 6px 10px rgba(0, 0, 0, 0.25)",
+              borderRadius: "50%",
+            }}
+            className=""
+            alt=""
+          />
+          <img
+            src={bola}
+            style={{
+              boxShadow: "4px 6px 10px rgba(0, 0, 0, 0.25)",
+              borderRadius: "50%",
+            }}
+            className=""
+            alt=""
+          />
+          <img
+            src={bola}
+            style={{
+              boxShadow: "4px 6px 10px rgba(0, 0, 0, 0.25)",
+              borderRadius: "50%",
+            }}
+            className=""
+            alt=""
+          />
+        </div>
+      </section>
     </div>
   );
 }
